b843d8bf5c2c372724efe444fe52978015e7938f
league <maddoxseul@sbsd.net>
2023-10-27 17:54:12 -0700

    asd;flkj

--- src/_01_array_list_visualizer/VisualArrayLists.java
+++ src/_01_array_list_visualizer/VisualArrayLists.java
@@ -21,19 +21,25 @@ public class VisualArrayLists implements ActionListener {
 		list.add("school");
 		list.add("water polo");
 		// 2. Set one item in our list to be something different
-		
+		list.set(2, "coding");
 		// 3. Remove something from our list
-		
+		list.remove(0);
 		// 4. Print the size of our list
-		
+		System.out.println(list.size());
 		// 5. Check if our list contains a specific thing of your choice
 		//	  and print whether it is in the list or not.
-		
+		if(list.contains("phone")) {
+			System.out.println("It's in the list");
+		}
+		else {
+			System.out.println("It's not in the list");
+		}
 		// 6. Get a specific item from our list and print it out
-		
+		System.out.println(list.get(1));
 		// 7. Clear our list of all items
-		
+		list.clear();
 		// 8. Print the size of our list again
+		System.out.println(list.size());
 	}
 	
 	
--- src/_02_array_list_guestbook/GuestBook.java
+++ src/_02_array_list_guestbook/GuestBook.java
@@ -1,6 +1,17 @@
 package _02_array_list_guestbook;
 
-public class GuestBook {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class GuestBook implements MouseListener, ActionListener {
 	// Create a GUI with two buttons. One button reads "Add Name" and the other button reads "View Names". 
 	// When the add name button is clicked, display an input dialog that asks the user to enter a name. Add
 	// that name to an ArrayList. When the "View Names" button is clicked, display a message dialog that displays
@@ -9,4 +20,71 @@ public class GuestBook {
 	// Guest #2: Sandy Summers
 	// Guest #3: Greg Ganders
 	// Guest #4: Donny Doners
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton button = new JButton();
+	JButton button1 = new JButton();
+	ArrayList<String> names = new ArrayList<String>();
+	
+	public void run() {
+		// TODO Auto-generated constructor stub
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(500,500);
+		frame.add(panel);
+		panel.add(button);
+		panel.add(button1);
+		button.setText("Add Name");
+		button1.setText("View Names");
+		frame.pack();
+		frame.addMouseListener(this);
+		button.addActionListener(this);
+		button1.addActionListener(this);
+		
+		
+		
+		
+	}
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+		
+		
+	}
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		if(button == arg0.getSource()) {
+			String input = JOptionPane.showInputDialog("Enter a Name");
+			names.add(input);
+		}
+		if(button1 == arg0.getSource()) {
+			System.out.println(names);
+		}
+		
+	}
+	
+	
+	
 }
